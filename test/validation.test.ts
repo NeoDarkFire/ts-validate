5ba2297bde3e18c8f21b02de0af5c5a339f1cff9
NeoDarkFire <dark.fire@gmx.fr>
2021-07-03 13:37:11 +0200

    added one more test

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { Validator, makeValidator, isString } from '~/index'
+import { Validator, makeValidator, isString, convert, toInt } from '~/index'
 
 test('simple validator with conditions', t => {
 	interface Mongo {
@@ -17,4 +17,18 @@ test('simple validator with conditions', t => {
 	// t.is(MongoValidator.match({ _id: '0123456789abcdef012345' }), false)
 	t.is(MongoValidator.match({ _id: 0x0123456789abcdef01234567 }), false)
 	t.is(MongoValidator.match({ _id: '0123456789abcdef01234567' }), true)
+});
+
+test('simple validator with conversion', t => {
+	interface SqlEntity {
+		id: number
+	}
+	const SqlValidator: Validator<SqlEntity> = makeValidator({
+		id: [convert(toInt), (x: number) => x > 0]
+	})
+	// TODO: why does this fail
+	// t.is(SqlValidator.match({ id: 0 }), false)
+	t.is(SqlValidator.match({ id: 1.5 }), true)
+	t.is(SqlValidator.match({ id: "4" }), true)
+	t.is(SqlValidator.match({ id: "abc" }), false)
 });
